e588eaaf94759c34afa875f82cc5b23bf39b7c01
Emre Kaçan <emrekacan@gmail.com>
2022-04-14 15:14:32 +0300

    hot fixing 4

--- pages/index.tsx
+++ pages/index.tsx
@@ -95,6 +95,8 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
 
+
+
             <Layout HeaderComponent={Header}>
                 <HomePage/>
             </Layout>
